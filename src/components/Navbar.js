f8654f05ebf1f81e1fb5deaa9a44fd6dd8fd2d36
JesusPimes <jesuskori@gmail.com>
2021-01-28 12:37:46 -0600

    comit encima del primer commit remoto

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,7 +35,7 @@ function Navbar() {
           </div>
         </div>
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-          <ul className='nav-menu-items' onClick={showSidebar}>
+          <ul className='navbar-right nav-menu-items' onClick={showSidebar}>
             <li className='navbar-toggle'>
               <div>
                <h5 className="colore">X</h5>
